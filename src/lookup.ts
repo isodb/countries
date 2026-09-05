097fbf8342c50074b312c2aaa4f0594cc4075326
komed3 <webmaster@komed3.de>
2026-09-05 14:14:40 +0200

    declare lookup maps

--- src/lookup.ts
+++ src/lookup.ts
@@ -4,3 +4,17 @@
 
 import { countries } from './data/countries';
 import type { Country } from './types';
+
+type LookupIndexes = {
+  alpha2?: ReadonlyMap< string, Country >;
+  alpha3?: ReadonlyMap< string, Country >;
+  numeric?: ReadonlyMap< string, Country >;
+  name?: ReadonlyMap< string, Country >;
+};
+
+type LookupKeys = {
+  alpha2?: ReadonlyArray< string >;
+  alpha3?: ReadonlyArray< string >;
+  numeric?: ReadonlyArray< string >;
+  name?: ReadonlyArray< string >;
+};
