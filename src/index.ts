b6d34425bb022eaddd06f3a47c846862630c7a62
komed3 <webmaster@komed3.de>
2026-09-05 14:37:41 +0200

    bind methods

--- src/index.ts
+++ src/index.ts
@@ -16,3 +16,18 @@ import { Lookup, lookup } from './lookup';
 
 export type * from './types';
 export { data, lookup };
+
+/**
+ * Finds a country by its ISO 3166-1 alpha-2 code.
+ */
+export const byAlpha2 = lookup.byAlpha2.bind( lookup );
+
+/**
+ * Finds a country by its ISO 3166-1 alpha-3 code.
+ */
+export const byAlpha3 = lookup.byAlpha3.bind( lookup );
+
+/**
+ * Finds a country by its ISO 3166-1 numeric code.
+ */
+export const byNumeric = lookup.byNumeric.bind( lookup );
