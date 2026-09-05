5546df5818901b45ace5a01a166a4c465ec5e3f5
komed3 <webmaster@komed3.de>
2026-09-05 14:36:37 +0200

    feat: create src/index.ts

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+/**
+ * Countries
+ * 
+ * Typed reference data for countries and their common
+ * geographic and international identifiers.
+ * 
+ * Provides immutable datasets and convenient lookup utilities
+ * for common country identifiers such as ISO codes and names.
+ * 
+ * @author Paul Köhler (komed3)
+ * @license MIT
+ */
