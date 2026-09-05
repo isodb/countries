4f04f388f0d5657d2a8073a63a1c9528e289bcb0
komed3 <webmaster@komed3.de>
2026-09-05 14:37:23 +0200

    manage imports

--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,9 @@
  * @author Paul Köhler (komed3)
  * @license MIT
  */
+
+import { countries as data } from './data/countries';
+import { Lookup, lookup } from './lookup';
+
+export type * from './types';
+export { data, lookup };
