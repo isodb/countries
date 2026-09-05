da5966de9ae3f5d2d6e0eb39eae5041d286cb038
komed3 <webmaster@komed3.de>
2026-09-05 10:49:42 +0200

    update contries.ts

--- src/data/contries.ts
+++ src/data/contries.ts
@@ -1,3 +1,23 @@
 import { Country } from '../types';
 
-export const countries = [] as const satisfies ReadonlyArray< Country >;
+export const countries = [ {
+  alpha2: 'DE',
+  alpha3: 'DEU',
+  numeric: '276',
+  name: 'Federal Republic of Germany',
+  officialName: 'Bundesrepublik Deutschland',
+  shortName: 'Germany',
+  continent: 'Europe',
+  capital: 'Berlin',
+  tel: '+49',
+  mobile: '262',
+  tld: '.de',
+  fips: 'GM',
+  nato: 'DE',
+  itu: 'D',
+  uicNumeric: '80',
+  uicAlpha: 'DE',
+  vehicleCode: 'D',
+  aircraftPrefix: 'D',
+  sailCode: 'GER'
+} ] as const satisfies ReadonlyArray< Country >;
